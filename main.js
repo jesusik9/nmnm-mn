b79b3bb41dc72d63780249bb1ccd5ce4ae443657
jesusik9 <121265760+jesusik9@users.noreply.github.com>
2023-07-21 13:53:36 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){mario_gameover=loadSound("gameover.wav");
+mario_jump=loadSound("jump.wav");
+mario_coin=loadSound("coin.wav");
+mario_kick=loadSound("kick.wav");
+mario_die=loadSound("mariodie.wav");
+world_start=loadSound("world_start.wav");
+setSprites();
+MarioAnimation();
+
+
+}
+function setup(){
+
+    
+}
